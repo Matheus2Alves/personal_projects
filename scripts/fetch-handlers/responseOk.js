3fbead550ccc122c5d6e2d184311c80b39e3edb5
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-07-20 17:04:41 -0300

    ok

--- scripts/fetch-handlers/responseOk.js
+++ scripts/fetch-handlers/responseOk.js
@@ -1,24 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btt = document.querySelector('button#btt')
-    btt.addEventListener('click', () => {
-
-        async function getImg() {
-            try {
-                const imgData = await fetch('dokOk.jpg')
-                if(!imgData.ok) {
-                    throw new Error('Network problem')
-                } else {
-                    const imgy = await imgData.blob()
-                    const myImgUrl = URL.createObjectURL(imgy)
-                    const myImg = document.querySelector('img#myImg')
-                    myImg.src = myImgUrl
-                    console.log('yeah')
-                } 
-            } catch(error) {
-                console.error('sorry', error)
-            }
+async function getData() {
+    const response = await fetch('https://api.github.com/users/omariosouto')
+    try{    
+        if(response.ok) {
+            console.log('tudo ok')
+            const Data = await response.json()
+            console.log(Data)
+        } else {
+            throw new Error('network problem')
         }
+    } catch(erro) {
+        console.error('my bad' , erro)
+    }
+}
 
-        getImg()
-    })
-})
+getData()
