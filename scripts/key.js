722408dae1ffe48f94f52e8676db2acef43ec117
Matheus <127268665+Matheus2Alves@users.noreply.github.com>
2023-07-30 19:28:09 -0300

    consuming API TMDB

--- scripts/key.js
+++ scripts/key.js
@@ -1,19 +1,3 @@
-async function getPopularMovies() {
-    const api_key = 'c63a77dd8778f83eb80305962f093ce5'
-    const base_url = 'https://api.themoviedb.org/3/'
-    const endPoint = 'movie/popular'
-    const url = `${base_url}${endPoint}?api_key=${api_key}`
-    try {
-        const response = await fetch(url)
-        if(!response.ok) {
-            throw new Error('network response was not ok')
-        } 
-        const data = await response.json()
-        return data
-    } catch (error){
-        console.error('error fatching data', error)
-    }
-}
+const api_key = 'c63a77dd8778f83eb80305962f093ce5'
 
-const processedData = getPopularMovies()
-export default processedData
+export default api_key
--- scripts/movie.js
+++ scripts/movie.js
@@ -1,59 +1,78 @@
+import { api_key } from "./key.js"
+
+async function getPopularMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_Key}&language=en-US&page=1`
+    const data = await fetch(url)
+    const { results } = await data.json()
+    return results
+}
+
+window.onload = async function() {
+    const movies  = await getPopularMovies()
+    movies.forEach(movie => displayMovies(movie))
+}
+
 const displayMovies = (movie) => {
+    const {title, poster_path, voto_average, release_date, overiew } = movie
+    const isFavorited = false //solves later
+
+    const year = new Date(release_date).getFullyear()
+    const image = `https://image.tmdb.org/t/p/w500${poster_path}`
+
     const movieContainer = document.querySelector('section.movies-container')
     const movieBox = document.createElement('article')
     movieBox.classList.add('movies')
     movieContainer.appendChild(movieBox)
+
     const moviePoster = document.createElement('div')
     moviePoster.classList.add('moviePoster')
     const movieImg = document.createElement('img')
     movieImg.classList.add('imgMoviePoster')
-    movieImg.src = movie.img
-    moviePoster.appendChild(movieImg)
-    //add content inside moviePoster
+    movieImg.src = image
+    movieImg.alt = `${title} Poster`
+    moviePoster.appendChild(movieImg)   
     movieBox.appendChild(moviePoster)
+
     const movieInfo = document.createElement('div')
     movieInfo.classList.add('infoMovie')
     const movieTitle = document.createElement('h2')
-    movieTitle.textContent = movie.title
-    movieInfo.appendChild(movieTitle)
-
-    //add content inside movieInfo
+    movieTitle.textContent = `${title} (${year})`
+    movieInfo.appendChild(movieTitle)       
     movieBox.appendChild(movieInfo)
+
     const movieStats = document.createElement('div')
     movieStats.classList.add('infostats')
     const rating = document.createElement('p')
-    const sRating = movie.rating
+    const sRating = voto_average
     const star = document.createElement('img')
     star.src = '../imagens/star.svg'
+    star.alt = 'star'
     rating.appendChild(star)
     rating.appendChild(document.createTextNode(sRating))
     movieStats.appendChild(rating)
+
     const favS = document.createElement('p') 
-    const favorite = movie.isFavorited ? 'disfavored' : 'favorite'
+    const favorite = isFavorited ? 'disfavored' : 'favorite'
     const favImg = document.createElement('img')
-    if(movie.isFavorited) {
-        favImg.src = '../imagens/fav.svg'
-    } else {
-        favImg.src = '../imagens/favnull.svg'
-    }
+    favImg.src = isFavorited ? '../imagens/fav.svg' : '../imagens/favnull.svg'
+    favImg.alt = 'Heart'
     favS.appendChild(favImg)
     favS.appendChild(document.createTextNode(favorite))
     movieStats.appendChild(favS)
-    //add content inside movieStats
     movieInfo.appendChild(movieStats)
+
     const movieDesc = document.createElement('div')
     movieDesc.classList.add('desc')
     const descMovie = document.createElement('p')
     const description = movie.description
-    descMovie.textContent = description
+    descMovie.textContent = overiew
     movieDesc.appendChild(descMovie)
-    //add content inside movieDesc
     movieBox.appendChild(movieDesc) 
 }
 
 
 
-const movies= [
+/*const movies= [
     {
         title: 'Oppenheimer',
         rating: 10,
@@ -85,4 +104,4 @@ const movies= [
 
 movies.forEach(movie => displayMovies(movie))
 
-/*img: '',*/
+img: '',*/
