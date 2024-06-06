c8cfc039ed8e858fee9d5f56393c725d545dee88
tainá tokumoto <tainatokumoto@gmail.com>
2024-06-06 19:21:00 -0300

    lala
    lala

--- documents/nietzscheProject/source/src/main.js
+++ documents/nietzscheProject/source/src/main.js
@@ -1,6 +1,10 @@
+
 document.querySelector('.btn ').addEventListener('click', function() {
     let  hedd = document.querySelectorAll('.darkwhite')
     window.alert("bla");
     this.classList.toggle('active');
     hedd.forEach(x => x.classList.toggle('active'));
 });
+
+
+
